858e5a25624c0817c2462f93f23f85a6bef01dbc
Gustavo Almeida Silva <gustavoaeida2002@gmail.com>
2022-07-18 23:05:09 -0300

    Initial commit

---
+++ main.R
@@ -0,0 +1,64 @@
+install.packages('seqinr')
+library('seqinr')
+
+#Using https://www.bioinformatics.org/sms2/random_dna.html website as DNA generator
+
+DNA=c("gctgctttccatcctacggtatttcacaactggcacaggccaataataagtgccactataccggtgagttcgaccaggcatctggcattccggtatcatatacagacggggttgtgaaacacgactccctttgattactctcttccaagacaagcgggatgactggcacgccgcggagtggaatcctctcgacggtttctttcagctttagtcccacccagtcgcttcattgatttttaggtccagacaccagtttgggcataagcaactgaattcgcacgagctgattggatcgctaccactgaagtgcgagtccaattggttgcggcgtcccgctcactctggtagaggatttatcgccatcaactaaagatcatattggactagtgacgagccagatctacctgttggttgtactctgataatctgttcagtatgggcagctcagaaagagcgtattagcccgaggaactcggtagggcgcttactgacggcccctcaaattaattgttccagatgtatcggcagcagtttctcactgctgacgtcggggcattgggtcccaagctcaagtccacttaagtttttgagttgttgctaaccgttacaggttgcgcagactcgtctgcgcagtcaagctcggtactggggctactagcgcctacacaccatataaggtcagatgtaaataggagaatgtagcagagcttgctgtagcaagtcgtgtgtataattctcctcttcaaggcctattgaaacaattgggacatgtatcgaacaacacgtaggtgtcaacccttctcgcagtagagaatttgggcccatacggagcgtttcaccacgggccatacacgtgacctcgcttatccgctcctctctactatggtagcatcgtgggagccgcgtgctccgcgatccgcgaatcgtgaataactggttataaatgaacgaccccaataatctctaaaagttctggtagatcacatacacgacttttttt")
+DNA=strsplit(DNA, split = "")
+DNA=DNA[[1]]
+
+
+## ---------------Among of function that returns the proportion of the nucleotides---------
+
+#1
+proportion=function(data_dna,word1,word2){
+  counts=0
+  total=0
+  
+  
+  for(i in 1:(length(data_dna)-1)){
+    if(data_dna[i]==word1)
+      {
+      total=total+1
+    if(data_dna[i+1]==word2)
+      counts=counts+1 
+    }
+  }
+  return(counts/total)
+}
+
+
+#2
+nucleo_finder=function(data_dna){
+  data_dna=levels(factor(data_dna))
+  return(data_dna)
+}
+
+#3 final
+nucleo_prob_matrix=function(data_dna){
+  nucleotides=nucleo_finder(data_dna)
+  nucleo_matrix=matrix(rep(0,length(nucleotides)**2),byrow = T,nrow=4)
+  
+  for(j in 1:length(nucleotides))
+    for(i in 1:length(nucleotides)){
+      
+      nucleo_matrix[i,j]=proportion(data_dna,nucleotides[j],nucleotides[i])
+    }
+  
+     
+return(nucleo_matrix)
+     
+    
+}
+
+nucleo_prob_matrix(DNA)
+
+
+
+
+
+
+
+
+
+
